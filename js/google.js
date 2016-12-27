108f198d8aa39333fdae0fd74697f8ae42d69a11
lillccian <lillccian@gmail.com>
2016-12-27 22:47:54 +0800

    add gps

--- js/google.js
+++ js/google.js
@@ -35,32 +35,32 @@ function initMap() {
 
   // find local gps and market it
 
-  // var infoWindow = new google.maps.InfoWindow({map: map});
-  // // Try HTML5 geolocation.
-  // if (navigator.geolocation) {
-  //   navigator.geolocation.getCurrentPosition(function(position) {
-  //     var pos = {
-  //       lat: position.coords.latitude,
-  //       lng: position.coords.longitude
-  //     };
-  //     marker = new google.maps.Marker({
-  //       map: map,
-  //       icon: image,
-  //       draggable: true,
-  //       animation: google.maps.Animation.DROP,
-  //       position: pos
-  //     });
-  //     marker.addListener('click', toggleBounce);
-  //     // infoWindow.setPosition(pos);
-  //     // infoWindow.setContent('Location found.');
-  //     map.setCenter(pos);
-  //   }, function() {
-  //     handleLocationError(true, infoWindow, map.getCenter());
-  //   });
-  // } else {
-  //   // Browser doesn't support Geolocation
-  //   handleLocationError(false, infoWindow, map.getCenter());
-  // }
+  var infoWindow = new google.maps.InfoWindow({map: map});
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: pos
+      });
+      marker.addListener('click', toggleBounce);
+      // infoWindow.setPosition(pos);
+      // infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 
 
   // gps
